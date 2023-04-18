ffe67034584bcc69e7a4728780bf4948a9bc26f4
DJDdawg <98183199+DJDdawg@users.noreply.github.com>
2023-04-17 22:39:58 -0500

    Add files via upload

---
+++ Random.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python
+
+import math
+import numpy as np
+
+#################
+# Random class
+#################
+# class that can generate random numbers
+class Random:
+    """A random number generator class"""
+
+    # initialization method for Random class
+    def __init__(self, seed = 5555):
+        self.seed = seed
+        self.m_v = np.uint64(4101842887655102017)
+        self.m_w = np.uint64(1)
+        self.m_u = np.uint64(1)
+        
+        self.m_u = np.uint64(self.seed) ^ self.m_v
+        self.int64()
+        self.m_v = self.m_u
+        self.int64()
+        self.m_w = self.m_v
+        self.int64()
+
+    # function returns a random 64 bit integer
+    def int64(self):
+        with np.errstate(over='ignore'):
+            self.m_u = np.uint64(self.m_u * np.uint64(2862933555777941757) + np.uint64(7046029254386353087))
+        self.m_v ^= self.m_v >> np.uint64(17)
+        self.m_v ^= self.m_v << np.uint64(31)
+        self.m_v ^= self.m_v >> np.uint64(8)
+        self.m_w = np.uint64(np.uint64(4294957665)*(self.m_w & np.uint64(0xffffffff))) + np.uint64((self.m_w >> np.uint64(32)))
+        x = np.uint64(self.m_u ^ (self.m_u << np.uint64(21)))
+        x ^= x >> np.uint64(35)
+        x ^= x << np.uint64(4)
+        with np.errstate(over='ignore'):
+            return (x + self.m_v)^self.m_w
+
+    # function returns a random floating point number between (0, 1) (uniform)
+    def rand(self):
+        return 5.42101086242752217E-20 * self.int64()
+
+    # function returns a random integer (0 or 1) according to a Bernoulli distr.
+    def Bernoulli(self, p=0.5):
+        if p < 0. or p > 1.:
+            return 1
+        
+        R = self.rand()
+
+        if R < p:
+            return 1
+        else:
+            return 0
+
+    #Exponential Distribution
+    def Exponential(self, Beta): #Beta is the scale parameter and is the inverse of the rate parameter
+      return np.random.exponential(scale=Beta, size=1)
+    
+    #create function that rolls a dice
+    def Categorical(self, p1, p2, p3, p4, p5):
+      R = self.rand(); #samples a random number R from a uniform distribution between 0 and 1. 
+    	
+      if R < p1:
+        return 1
+      if R < p1 + p2: 
+        return 2
+      if R < p1 + p2 + p3:
+        return 3
+      if R < p1 + p2 + p3 + p4:
+        return 4
+      if R < p1 + p2 + p3 + p4 + p5:
+        return 5
+      else:
+        return 6
+      
+    #Gaussian Distribution  
+    def Gaussian(self, mean, sigma):
+    	return np.random.normal(loc=mean, scale=sigma, size=1)
